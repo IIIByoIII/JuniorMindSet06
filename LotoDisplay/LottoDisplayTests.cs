fa891dabf6fdc83d34b1d4f77d3d7a9dd7020dae
IIIByoIII <robertkiss26@gmail.com>
2017-11-13 20:21:31 +0200

    Added new LottoDisplay project
    In a new solution (repository)

--- LotoDisplay/LottoDisplayTests.cs
+++ LotoDisplay/LottoDisplayTests.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace LotoDisplay
+namespace LottoDisplay
 {
     [TestClass]
-    public class UnitTest1
+    public class LottoDisplayTests
     {
         [TestMethod]
         public void TestMethod1()
