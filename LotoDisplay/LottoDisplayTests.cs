bf1885de39202f0a66d225871a431f6601e324ae
IIIByoIII <robertkiss26@gmail.com>
2017-11-21 20:10:51 +0200

    Added LottoFirst3 test
    Also inserted a space after the last number for proper formatting

--- LotoDisplay/LottoDisplayTests.cs
+++ LotoDisplay/LottoDisplayTests.cs
@@ -35,14 +35,21 @@ namespace LottoDisplay
         public void LottoAllNumbers()
         {
             int[] numbers = { 35, 43, 40, 5, 3, 29 };
-            CollectionAssert.AreEqual(new int[] { 3, 5, 29, 35, 40, 43}, LottoNumbers(numbers, 6));
+            CollectionAssert.AreEqual(new int[] { 3, 5, 29, 35, 40, 43 }, LottoNumbers(numbers, 6));
         }
 
         [TestMethod]
         public void LottoFirst5()
         {
             int[] numbers = { 35, 43, 40, 5, 3, 29 };
-            CollectionAssert.AreEqual(new int[] { 3, 5, 35, 40, 43}, LottoNumbers(numbers, 5));
+            CollectionAssert.AreEqual(new int[] { 3, 5, 35, 40, 43 }, LottoNumbers(numbers, 5));
+        }
+
+        [TestMethod]
+        public void LottoFirst3()
+        {
+            int[] numbers = { 35, 43, 40, 5, 3, 29 };
+            CollectionAssert.AreEqual(new int[] { 35, 40, 43 }, LottoNumbers(numbers, 3));
         }
 
         int[] LottoNumbers(int[] numbers, int toShow)
