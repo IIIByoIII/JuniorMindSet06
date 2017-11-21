691d0ed163ec28f0aa42469f716b3ea07e1a5370
IIIByoIII <robertkiss26@gmail.com>
2017-11-21 20:07:50 +0200

    Added LottoFirst5 test

--- LotoDisplay/LottoDisplayTests.cs
+++ LotoDisplay/LottoDisplayTests.cs
@@ -38,6 +38,13 @@ namespace LottoDisplay
             CollectionAssert.AreEqual(new int[] { 3, 5, 29, 35, 40, 43}, LottoNumbers(numbers, 6));
         }
 
+        [TestMethod]
+        public void LottoFirst5()
+        {
+            int[] numbers = { 35, 43, 40, 5, 3, 29 };
+            CollectionAssert.AreEqual(new int[] { 3, 5, 35, 40, 43}, LottoNumbers(numbers, 5));
+        }
+
         int[] LottoNumbers(int[] numbers, int toShow)
         {
             int[] result = new int[toShow];
