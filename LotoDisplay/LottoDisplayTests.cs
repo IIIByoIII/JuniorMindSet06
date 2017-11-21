420a344e3ea058dfd8694feadf3cbce43365d234
IIIByoIII <robertkiss26@gmail.com>
2017-11-21 19:46:04 +0200

    Added AscendingNumbers test
    Using Insertion Sorting algorithm

--- LotoDisplay/LottoDisplayTests.cs
+++ LotoDisplay/LottoDisplayTests.cs
@@ -7,8 +7,28 @@ namespace LottoDisplay
     public class LottoDisplayTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AscendingNumbers()
         {
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4, 5 }, InsertionSort(new int[] { 3, 2, 5, 1, 4 }));
+        }
+
+        int[] InsertionSort(int[] nr, int j = 1, int k = 1)
+        {
+            if (k == nr.Length)
+                return nr;
+            if (j == 0) {
+                k++;
+                return InsertionSort(nr, k, k);
+            }
+            if (nr[j] < nr[j-1]) {
+                int t = nr[j-1];
+                nr[j-1] = nr[j];
+                nr[j] = t;
+                j--;
+                return InsertionSort(nr, j, k);
+            }
+            k++;
+            return InsertionSort(nr, k, k);
         }
     }
 }
