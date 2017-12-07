8f83865157b64bf6ecd33a5ab501da665314a68b
IIIByoIII <robertkiss26@gmail.com>
2017-12-07 20:04:50 +0200

    Added SortedOrdersAndPriorities test

--- ServiceCenter/ServiceOrders.cs
+++ ServiceCenter/ServiceOrders.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sorting;
 
 namespace ServiceCenter
 {
     public class Service
     {
+        public static int[,] OrderedPriorities(int[] pr)
+        {
+            int[,] result = Orders(pr);
+            return Insertion.Sort2D(result, 1);
+        }
+
         public static int[,] Orders(int[] pr)
         {
             int[,] result = new int[pr.Length, 2];
--- ServiceCenter/ServiceOrdersTests.cs
+++ ServiceCenter/ServiceOrdersTests.cs
@@ -12,5 +12,13 @@ namespace ServiceCenter
             int[] priorities = new int[] { 2, 1, 3 };
             CollectionAssert.AreEqual(new int[,]{{ 1, 2 }, { 2, 1 }, { 3, 3 }}, Service.Orders(priorities));
         }
+
+        [TestMethod]
+        public void SortedOrdersAndPriorities()
+        {
+            int[] priorities = new int[] { 2, 1, 3, 2, 1 };
+            int[,] result = new int[,] {{ 2, 1 }, { 5, 1 }, { 1, 2 }, { 4, 2 }, { 3, 3 }}; 
+            CollectionAssert.AreEqual(result, Service.OrderedPriorities(priorities));
+        }
     }
 }
