528803c2369e01a5973386815c0fa81a688c3beb
IIIByoIII <robertkiss26@gmail.com>
2017-12-20 21:17:14 +0200

    Added SortedStructOrders test
    Along with Order struct and OrderedStruct method. (it's a makeshift solution to use already existing functions to not modify the sorting algorithm in handling a struct, I know ... lazyness FTW)

--- ServiceCenter/ServiceOrders.cs
+++ ServiceCenter/ServiceOrders.cs
@@ -7,8 +7,32 @@ using Sorting;
 
 namespace ServiceCenter
 {
+    public struct Order
+    {
+        public int pr;
+        public string name;
+
+        public Order(int priority, string clientName)
+        {
+            pr = priority;
+            name = clientName;
+        }
+    }
+
     public class Service
     {
+        public static Order[] OrderedStruct(Order[] orders)
+        {
+            int[] priorities = new int[orders.Length];
+            for (int i = 0; i < orders.Length; i++)
+                priorities[i] = orders[i].pr;
+            int[,] prioritiesOrder = OrderedPriorities(priorities);
+            Order[] result = new Order[orders.Length];
+            for (int i = 0; i < orders.Length; i++)
+                result[i] = orders[prioritiesOrder[i,0] - 1];
+            return result;
+        }
+
         public static int[,] OrderedPriorities(int[] pr)
         {
             int[,] result = Orders(pr);
--- ServiceCenter/ServiceOrdersTests.cs
+++ ServiceCenter/ServiceOrdersTests.cs
@@ -20,5 +20,18 @@ namespace ServiceCenter
             int[,] result = new int[,] {{ 2, 1 }, { 5, 1 }, { 1, 2 }, { 4, 2 }, { 3, 3 }}; 
             CollectionAssert.AreEqual(result, Service.OrderedPriorities(priorities));
         }
+
+        [TestMethod]
+        public void SortedStructOrders()
+        {
+            Order ord1 = new Order(2, "John");
+            Order ord2 = new Order(1, "Janice");
+            Order ord3 = new Order(3, "Joe");
+            Order ord4 = new Order(2, "Jesse");
+            Order ord5 = new Order(1, "James");
+            Order[] orders = new Order[] { ord1, ord2, ord3, ord4, ord5 };
+            Order[] result = new Order[] { ord2, ord5, ord1, ord4, ord3 };
+            CollectionAssert.AreEqual(result, Service.OrderedStruct(orders));
+        }
     }
 }
